9c19e639728abcf8cc8f78be2b9c595a05becb99
Anshul Kushwaha <anshul@Anshuls-MacBook-Air.local>
2018-01-16 02:41:43 +0530

    Db migration changes

--- db/migrate/20180111092141_add_indexes_to_email_logs.rb
+++ db/migrate/20180111092141_add_indexes_to_email_logs.rb
@@ -1,6 +1,6 @@
 class AddIndexesToEmailLogs < ActiveRecord::Migration[5.1]
   def change
-    add_index :email_logs, :post_id
+    add_index :email_logs, :post_id, :null => false, :default => 0
     add_index :email_logs, :topic_id
   end
 end
--- db/migrate/20180115201643_add_solutions_to_user_stats.rb
+++ db/migrate/20180115201643_add_solutions_to_user_stats.rb
@@ -1,6 +1,6 @@
 class AddSolutionsToUserStats < ActiveRecord::Migration[5.1]
   def change
-    add_column :user_stats, :solutions, :int
+    add_column :user_stats, :solutions, :int, :null => false, :default => 0
     add_index :user_stats, :solutions
   end
 end
